2b33b108d3f60406dc007f5dfaaf1c07c0f9f4a9
PK <pradeepdhital124@gmail.com>
2023-02-01 21:18:29 +1100

    worked on database schema and module and they are called on the router

--- api/src/model/task/taskModel.js
+++ api/src/model/task/taskModel.js
@@ -1,2 +1,26 @@
-import taskSchema from "./taskSchema"; 
-// insert 
+import taskSchema from "./taskSchema.js"; 
+// insert
+export const insertTask = taskObj =>{
+   return taskSchema(taskObj).save()
+}
+
+
+// select
+
+
+export const getTasks = () => {
+  return taskSchema.find();
+};
+
+export const getsingleTask = (_id) => {
+  return taskSchema.findById(_id);
+};
+
+
+
+// update
+
+
+
+
+// delete
--- api/src/routers/taskRouter.js
+++ api/src/routers/taskRouter.js
@@ -1,35 +1,29 @@
 import express from "express";
+import { getsingleTask, getTasks, insertTask } from "../model/task/taskModel.js";
 const router = express.Router();
-let fakeDB = [
-  { _id: 1, task: "coding", hr: "5" },
-  { _id: 2, task: "sleeping", hr: "8" },
-  { _id: 3, task: "working", hr: "8" },
-];
-router.get("/:_id?", (req, res, next) => {
+
+router.get("/:_id?", async(req, res, next) => {
   try {
     const { _id } = req.params;
-    if (_id) {
-      data = fakeDB.filter((item) => item._id === +_id);
-      fakeDB = data;
-    }
-
+   const result=_id? await getsingleTask(_id):await getTasks()
     res.json({
       status: "success",
       message: "response from  get method",
-      fakeDB,
+      result
     });
   } catch (error) {
     error.status = 500;
     next(error);
   }
 });
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   try {
     //  call db query to add data to the database
-    fakeDB.push(req.body);
+    const result=await insertTask(req.body);
     res.json({
       status: "success",
       message: "response from  post method",
+      result,
     });
   } catch (error) {
     next(error);
@@ -47,10 +41,7 @@ router.patch("/", (req, res) => {
 });
 router.delete("/:_id", (req, res, next) => {
   try {
-    const { _id } = req.params;
-    // db query to delete data
-    const filteredArg = fakeDB.filter((item) => item?._id !== +_id);
-    fakeDB = filteredArg;
+  
     res.json({
       status: "success",
       message: "response from  delete method",
